4c33639af6b10c929212de0d07e1b86f3dd217c1
Longjing Jiang <longjinj@uci.edu>
2026-02-15 22:07:52 -0800

    retrievied user's liked songs

--- backend/song-details.ts
+++ backend/song-details.ts
@@ -3,9 +3,9 @@ import { GoogleGenAI } from '@google/genai';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-const ai = new GoogleGenAI({});
+const ai = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
 
-async function getEnergyRating(trackID: string) {
+export async function getEnergyRating(trackID: string) {
     // SPOTIFY STUFF - GET SONG NAME/ARTIST
     const clientID = process.env.CLIENT_ID;
     const clientSecret = process.env.CLIENT_SECRET;
@@ -13,8 +13,8 @@ async function getEnergyRating(trackID: string) {
     const song = await sdk.tracks.get(trackID);
     const songName = song["name"];
     const artistName = song["artists"][0]["name"];
-    // console.log(song["name"]);
-    // console.log(song["artists"][0]["name"]);
+    console.log(song["name"]);
+    console.log(song["artists"][0]["name"]);
 
     // GEMINI API
     const rating = await ai.models.generateContent({
@@ -23,8 +23,9 @@ async function getEnergyRating(trackID: string) {
                     from 0 to 1, 0 being low energy and 1 being high energy.
                     Give only the rating.`
     });
-    // console.log(rating.text);
+    console.log(rating.text);
     return parseFloat(rating.text!);
 }
 
-// getEnergyRating("02sy7FAs8dkDNYsHp4Ul3f");
+getEnergyRating("02sy7FAs8dkDNYsHp4Ul3f");
+

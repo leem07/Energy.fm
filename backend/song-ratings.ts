b5164da23c900be7c1796eef7a4615bae94ff1d2
Michelle Lee <michelles.lee3153@gmail.com>
2026-02-25 14:12:21 -0800

    JSON created and added

--- backend/song-ratings.ts
+++ backend/song-ratings.ts
@@ -1,9 +1,13 @@
 import { SpotifyApi } from '@spotify/web-api-ts-sdk';
 import { GoogleGenAI } from '@google/genai';
 import * as dotenv from 'dotenv';
+import { tracingChannel } from 'node:diagnostics_channel';
 
 dotenv.config();
 const ai = new GoogleGenAI({});
+interface Song { rating: string; trackID: string};
+let songJson: Record<string, Song[]> = { "0.0": [], "0.1": [], "0.2": [], "0.3": [], "0.4": [], 
+                "0.5": [], "0.6": [], "0.7": [], "0.8": [], "0.9": [] };
 
 async function getEnergyRating(trackID: string) {
     // SPOTIFY STUFF - GET SONG NAME/ARTIST
@@ -16,15 +20,27 @@ async function getEnergyRating(trackID: string) {
     // console.log(song["name"]);
     // console.log(song["artists"][0]["name"]);
 
-    // GEMINI API
-    const rating = await ai.models.generateContent({
+    // GEMINI API - GET ENERGY SCORE
+    const response = await ai.models.generateContent({
         model: "gemini-3-flash-preview",
         contents: `Rate the song ${songName} by ${artistName} for its energy 
                     from 0 to 1, 0 being low energy and 1 being high energy.
                     Give only the rating.`
     });
-    // console.log(rating.text);
-    return parseFloat(rating.text!);
+    const rating: string = response.text!;
+    // console.log(rating);
+
+    // Return rating and song ID
+    return [rating, trackID];
 }
 
-// getEnergyRating("02sy7FAs8dkDNYsHp4Ul3f");
+async function addSong(rating: string, trackID: string) {
+    songJson[rating.substring(0,3)].push({rating, trackID});
+
+    console.log(songJson);
+}
+
+// getEnergyRating("4Oih3RDrSFg3afaOphBVuy");
+
+// addSong("0.29", "4Oih3RDrSFg3afaOphBVuy");
+// addSong("0.49", "ljwf39fsjklw3lrkj3213l");

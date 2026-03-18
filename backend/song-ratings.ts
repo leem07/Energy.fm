08500c0079df537043ae9ff6e3c2ded65d520d77
Michelle Lee <michelles.lee3153@gmail.com>
2026-03-18 15:45:20 -0700

    Added bestMatch function

--- backend/song-ratings.ts
+++ backend/song-ratings.ts
@@ -2,6 +2,8 @@ import { SpotifyApi } from '@spotify/web-api-ts-sdk';
 import { GoogleGenAI } from '@google/genai';
 import * as dotenv from 'dotenv';
 import { tracingChannel } from 'node:diagnostics_channel';
+// import songJson from './songs.json' assert { type: 'json'};
+import { readFile, writeFile } from 'fs/promises';
 
 dotenv.config();
 const ai = new GoogleGenAI({});
@@ -25,8 +27,36 @@ export function addSong(songJson: Record<string, Song[]>, rating: string, trackI
     songJson[rating.substring(0,3)].sort((a,b) => {
         return parseFloat(a.rating) - parseFloat(b.rating);
     })
-    // console.log(songJson);
 }
+
+export async function getBestMatch(heartRate: number) {
+    // Read JSON file
+    const raw = await readFile("./backend/songs.json", "utf8");
+    const obj = JSON.parse(raw);
+
+    // Search through index
+    let index = String(heartRate).substring(0,3);
+    const arr = obj.buckets[index];
+    let bestFit = arr[0];
+
+    // Empty index
+    // if (arr.length == 0) {
+        // for (let i = index; i )
+    // }
+
+    // else {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].rating >= heartRate) {
+                bestFit = arr[i].trackID;
+                break;
+            }
+        }
+    // }
+    
+    return bestFit;
+}
+
+// export function getHighMatch(songJson: Record)
 
 /* API FUNCTIONS */
 export async function getSongDetails(trackID: string) {
@@ -62,3 +92,6 @@ export async function getEnergyRating(songName: string, artistName: string) {
 // addSong(json, "0.29", "4Oih3RDrSFg3afaOphBVuy");
 // addSong(json, "0.22", "a3wjlv9zaa3l3wrcs9lwk3");
 // addSong(json, "0.49", "ljwf39fsjklw3lrkj3213l");
+// console.log(json);
+// console.log(readJson());
+// getBestMatch(0.35);
